7f915c57866deb585d97d72387912ffe22221a24
gregory hanffou <ghanffou@gmail.com>
2018-06-19 13:49:00 +0200

    index page for mobile and seed with 3 experiences

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,10 @@ hostjoe = HostInfo.create!(name:"Joe", description:"canal tour provider", user: 
 referrerbarbara = ReferrerInfo.create!(user: barbara, referral_code: "00001")
 
 canaltour = Experience.create!(name: "Joes Canal Tour", price:"50", description:"much tour, very wow!", host_info:hostjoe, location: amsterdam, photo:"https://res.cloudinary.com/greggstyle/image/upload/v1529099386/xipqak07dqbfpwrqopaq.jpg")
+canaltours = Experience.create!(name: "B Canal Tour", price:"40", description:"much tour, very wow!", host_info:hostjoe, location: amsterdam, photo:"https://res.cloudinary.com/greggstyle/image/upload/v1529099386/xipqak07dqbfpwrqopaq.jpg")
+canaltourrrrr = Experience.create!(name: "H Canal Tour", price:"30", description:"much tour, very wow!", host_info:hostjoe, location: amsterdam, photo:"https://res.cloudinary.com/greggstyle/image/upload/v1529099386/xipqak07dqbfpwrqopaq.jpg")
+
+
 
 carlasbooking = Booking.create!(referrer_info: referrerbarbara, traveller_info: travellercarla, status:"pending", amount:"1", ngo: redcross, experience: canaltour)
 
