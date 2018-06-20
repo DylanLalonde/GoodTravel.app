a75128ebc3fd2a6e5578d5175e8e42a8f0a031e9
Dylan Lalonde <contact@dylanlalonde.com>
2018-06-20 17:12:09 +0200

    small javascript fix to peopleField

--- app/javascript/components/booking_amount.js
+++ app/javascript/components/booking_amount.js
@@ -1,12 +1,14 @@
 function setAmount () {
   const peopleField = document.querySelector("#search_number_traveller");
-  peopleField.addEventListener('change', () => {
-    const people = peopleField.value;
-    const Amount = document.getElementById('amount-experience').innerText;
-    const totalCost = Amount * people
-    const amountField = document.querySelector('#search_amount')
-    amountField.value = totalCost
-  })
+    if (peopleField) {
+      peopleField.addEventListener('change', () => {
+        const people = peopleField.value;
+        const Amount = document.getElementById('amount-experience').innerText;
+        const totalCost = Amount * people;
+        const amountField = document.querySelector('#search_amount');
+        amountField.value = totalCost;
+      });
+    }
 }
 
 export { setAmount };
