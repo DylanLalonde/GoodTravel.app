8a2a63d6161c8aff9749257675b6a001af0e758f
Jason Everaert <eventlessdrop8@gmail.com>
2018-06-25 14:32:44 +0200

    Sorting out merge

--- db/migrate/20180625101443_change_amount_in_bookings.rb
+++ db/migrate/20180625101443_change_amount_in_bookings.rb
@@ -1,6 +1,6 @@
 class ChangeAmountInBookings < ActiveRecord::Migration[5.2]
   def change
-    remove_column :bookings, :amount
+    # remove_column :bookings, :amount
     add_monetize :bookings, :amount, currency: { present: false }
   end
 end
