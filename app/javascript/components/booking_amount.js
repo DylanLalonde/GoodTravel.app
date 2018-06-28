54f31e0c100f0f9ba5c670792d1234276fc92e52
gregory hanffou <ghanffou@gmail.com>
2018-06-28 18:48:45 +0200

    donation to your ngo on booking page

--- app/javascript/components/booking_amount.js
+++ app/javascript/components/booking_amount.js
@@ -6,7 +6,10 @@ function setAmount() {
             const Amount = document.getElementById('amount-experience').innerText;
             console.log(Amount);
             const totalCost = `${Amount * people}`;
+            const donationAmount = totalCost * 0.05;
             console.log(totalCost)
+            const amountDonationField = document.getElementById('donation-amount');
+            amountDonationField.innerHTML = donationAmount;
             const amountField = document.querySelector('#booking_amount_cents');
             amountField.value = totalCost;
         });
